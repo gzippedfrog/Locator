257b102001a9c20f4436f5e4e9259d2b94aee688
gzippedfrog <gzippedfrog@outlook.com>
2022-03-03 15:33:49 +0300

    added mobx

--- src/App.js
+++ src/App.js
@@ -1,94 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Text, TextInput } from "react-native-paper";
-import publicIP from "react-native-public-ip";
+import { observer } from "mobx-react-lite";
+import locator from "./store/locator";
 
-var token = "be058ccf4fffe4b8bed23887850577b5d034354f";
-var url1 =
-  "https://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address?ip=";
-var options = {
-  method: "GET",
-  mode: "cors",
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    Authorization: "Token " + token
-  }
-};
-
-const App = () => {
-  const [ip, setIp] = useState(null);
-  const [city, setCity] = useState(null);
-  const [street, setStreet] = useState(null);
-  const [suggestions, setSuggestions] = useState([]);
-
-  useEffect(() => {
-    publicIP()
-      .then((ip) => {
-        setIp(ip);
-        return fetch(url1 + ip, options);
-      })
-      .then((response) => response.json())
-      .then((data) => setCity(data.location.data.city))
-      .catch((error) => console.log("error", error));
-  }, []);
-
-  var url2 =
-    "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
-  var options2 = {
-    method: "POST",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Authorization: "Token " + token
-    },
-    body: JSON.stringify({ query: city + " " + street })
-  };
-  function handleStreetChange(text) {
-    /[^A-Za-zА-Яа-я]+/.test(text) || setStreet(text);
+const App = observer(() => {
+  function handleStreetChange(street) {
+    /[^A-Za-zА-Яа-я]+/.test(street) || locator.setStreet(street);
   }
 
   function handleStreetBlur() {
-    if (!street) return;
-    fetch(url2, options2)
-      .then((response) => response.json())
-      .then(({ suggestions }) => {
-        setSuggestions(suggestions);
-      })
-      .catch((error) => console.log("error", error));
+    locator.fetchSuggestions();
   }
 
   return (
     <View style={styles.container}>
       <TextInput
         label="IP адрес"
-        value={ip}
+        value={locator.ip}
         disabled
         style={styles.textInput}
       />
 
-      <TextInput label="Город" value={city} disabled style={styles.textInput} />
+      <TextInput
+        label="Город"
+        value={locator.city}
+        disabled
+        style={styles.textInput}
+      />
 
       <TextInput
         label="Улица"
-        value={street}
+        value={locator.street}
         onChangeText={handleStreetChange}
         style={styles.textInput}
         onBlur={handleStreetBlur}
       />
 
-      {suggestions.map((item) => (
+      {locator.suggestions?.map((item) => (
         <Text key={item.value}>{item.value}</Text>
       ))}
     </View>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 10,
-    marginTop: 20
+    flex: 1
   },
   textInput: {
     marginBottom: 10
---
+++ src/store/locator.js
@@ -0,0 +1,67 @@
+import { action, makeAutoObservable } from "mobx";
+import publicIP from "react-native-public-ip";
+
+const token = "be058ccf4fffe4b8bed23887850577b5d034354f";
+const api_url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/";
+
+class Locator {
+  ip = null;
+  city = null;
+  street = null;
+  suggestions = [];
+
+  constructor() {
+    makeAutoObservable(this);
+
+    publicIP().then(
+      action((ip) => {
+        this.ip = ip;
+
+        fetch(api_url + "iplocate/address?ip=" + ip, {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Token " + token
+          }
+        })
+          .then((response) => response.json())
+          .then(
+            action(({ location }) => {
+              this.city = location.data.city;
+            })
+          )
+          .catch((error) => console.log("error", error));
+      })
+    );
+  }
+
+  setStreet(street) {
+    this.street = street;
+  }
+
+  fetchSuggestions() {
+    const query = this.city + " " + this.street;
+
+    fetch(api_url + "suggest/address", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: "Token " + token
+      },
+      body: JSON.stringify({ query })
+    })
+      .then((response) => response.json())
+      .then(
+        action(({ suggestions }) => {
+          this.suggestions = suggestions;
+        })
+      )
+      .catch((error) => console.log("error", error));
+  }
+}
+
+export default new Locator();
